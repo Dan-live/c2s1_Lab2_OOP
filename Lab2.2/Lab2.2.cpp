c228af3a69d0ad9414032f65bfb7473ae297baaa
Dan-live <109356212+Dan-live@users.noreply.github.com>
2022-10-27 18:51:51 +0300

    Add files via upload

---
+++ Lab2.2/Lab2.2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include "Geometric_Figures.h"
+
+
+
+int main()
+{
+    Icosahedron tr(10);
+   
+    tr.Print_all_Data();
+
+    cout << "------------------------------------------------" << endl;
+   // tr.Copy_Metod(&tr);
+   
+    
+    
+    /*tr.Set_length(1);
+    tr.Print_all_Data();*/
+}
+
